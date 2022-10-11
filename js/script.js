586b42e740a10f37edd6fc1a95ef543ef2044dc9
steffen kroyer <stef5450@stud.kea.dk>
2022-10-11 13:00:26 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
+
+let menu = document.querySelector("#menu-bars");
+
+let cursor1 = document.querySelector(".cursor-1");
+let cursor2 = document.querySelector(".cursor-2");
+
+window.onmousemove = (e) => {
+  cursor1.style.top = e.pageY + "px";
+  cursor1.style.left = e.pageX + "px";
+  cursor2.style.top = e.pageY + "px";
+  cursor2.style.left = e.pageX + "px";
+};
